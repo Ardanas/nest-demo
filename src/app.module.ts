ebc9156d4391ebb872918a4cd2082f7c594e65d9
Ardans <719481334@qq.com>
2021-12-01 23:11:38 +0800

    --add--graphql的支持

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { AppResolver } from './app.resolver';
+// import { join } from 'path';
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot({
+      installSubscriptionHandlers: true,
+      autoSchemaFile: 'schema.gql', // graphql@15.7.2 ok
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, AppResolver],
 })
 export class AppModule {}
